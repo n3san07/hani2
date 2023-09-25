a396e1c76153b215ac25f2fd6b1bc302d1d4a252
n3san07 <hanigat7@gmail.com>
2023-09-25 16:27:34 +0300

    fixing loading dark mode

--- CLIENT/src/providers/Loading.jsx
+++ CLIENT/src/providers/Loading.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { RingLoader } from "react-spinners";
-import { Grid } from "@mui/material";
+import { Grid, Paper } from "@mui/material";
 
 const Loading = () => {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: "80vh" }}
-    >
-      <Grid item xs={3}>
-        <RingLoader color="#ff9e45" size={300} />
+    <Paper>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "80vh" }}
+      >
+        <Grid item xs={3}>
+          <RingLoader color="#ff9e45" size={300} />
+        </Grid>
       </Grid>
-    </Grid>
+    </Paper>
   );
 };
 
